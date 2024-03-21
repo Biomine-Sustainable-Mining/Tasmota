3586e21b51eda1a01e9ef323c084eb4a0f363450
bicodrex <127663368+bicodrex@users.noreply.github.com>
2024-03-21 18:23:01 +0100

    empty drv created

---
+++ tasmota/tasmota_xdsp_display/xdsp_21_waveshare.ino
@@ -0,0 +1,58 @@
+/*
+  xdsp_05_epaper.ino - Display e-paper support for Tasmota
+
+  Copyright (C) 2021  Theo Arends, Gerhard Mutz and Waveshare
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifdef USE_SPI
+#ifdef USE_DISPLAY
+#ifdef USE_DISPLAY_WAVESHARE
+
+#define XDSP_21                21
+
+#define COLORED42              1
+#define UNCOLORED42            0
+
+// using font 8 is opional (num=3)
+// very badly readable, but may be useful for graphs
+#define USE_TINY_FONT
+
+
+/*********************************************************************************************\
+ * Interface
+\*********************************************************************************************/
+
+bool Xdsp21(uint32_t function)
+{
+  Serial.println("IT IS LOADED");
+  bool result = false;
+
+  if (FUNC_DISPLAY_INIT_DRIVER == function) {
+  }
+  else if (epd42_init_done && (XDSP_06 == Settings->display_model)) {
+    switch (function) {
+      case FUNC_DISPLAY_MODEL:
+        result = true;
+        break;
+    }
+  }
+  return result;
+}
+
+#endif  // USE_DISPLAY_EPAPER42
+#endif  // USE_DISPLAY
+#endif  // USE_SPI
