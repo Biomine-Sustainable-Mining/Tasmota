d5fdeca23a47963a087191047c3f1f553ec33ecc
bicodrex <127663368+bicodrex@users.noreply.github.com>
2024-03-24 12:15:39 +0100

    test: barebone output, spi only

--- tasmota/tasmota_xdsp_display/xdsp_21_waveshare.ino
+++ tasmota/tasmota_xdsp_display/xdsp_21_waveshare.ino
@@ -17,25 +17,22 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-
 #ifdef USE_SPI
 #ifdef USE_DISPLAY
 #ifdef USE_DISPLAY_WAVESHARE
 
-#define XDSP_21                21
+#define XDSP_21 21
 
-#define COLORED42              1
-#define UNCOLORED42            0
+#define COLORED42 1
+#define UNCOLORED42 0
 
 // using font 8 is opional (num=3)
 // very badly readable, but may be useful for graphs
 #define USE_TINY_FONT
 
-
 #define GxEPD2_DISPLAY_CLASS GxEPD2_BW
 
 #define GxEPD2_DRIVER_CLASS GxEPD2_750_T7
-
 
 #include <GxEPD2_BW.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
@@ -47,37 +44,71 @@
 
 bool Xdsp21(uint32_t function)
 {
-  Serial.println("IT IS LOADED");
-  const char HelloWorld[] = "Biomine S.r.l.";
-
-  bool result = false;
-
-  display.init(9600, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
-  
-  display.setRotation(0);
-  display.setFont(&FreeMonoBold9pt7b);
-
-  display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
-  // center the bounding box by transposition of the origin:
-  uint16_t x = ((display.width() - tbw) / 2) - tbx;
-  uint16_t y = ((display.height() - tbh) / 2) - tby;
-  display.setFullWindow();
-
-  display.firstPage(); //FILL WHITE
-  do
+  AddLog(LOG_LEVEL_INFO, PSTR("DSP: Waveshare Driver"));
+  if (PinUsed(GPIO_WAVESHARE_CS) && ((TasmotaGlobal.soft_spi_enabled & SPI_MOSI) || (TasmotaGlobal.spi_enabled & SPI_MOSI)))
   {
-    //display.drawRect(x-10,y+10, 200, -50, GxEPD_BLACK);
-    display.setCursor(x,y);
-    display.print(HelloWorld);
+    Serial.println("CS Selected");
+
+    if (TasmotaGlobal.soft_spi_enabled)
+    {
+      Serial.println("SoftSPI");
+      Serial.print("CS: ");
+      Serial.println(Pin(GPIO_WAVESHARE_CS));
+      Serial.print("MOSI: ");
+      Serial.println(Pin(GPIO_SSPI_MOSI));
+      Serial.print("SCLK: ");
+      Serial.println(Pin(GPIO_SSPI_SCLK));
+      Serial.print("DC: ");
+      Serial.println(Pin(GPIO_SSPI_DC));
+      Serial.print("BUSY/MISO: ");
+      Serial.println(Pin(GPIO_SSPI_MISO));
+      Serial.print("Rst: ");
+      Serial.println(Pin(GPIO_OLED_RESET));
+    }
+    else if (TasmotaGlobal.spi_enabled)
+    {
+      Serial.println("HardSPI");
+      Serial.print("CS: ");
+      Serial.println(Pin(GPIO_WAVESHARE_CS));
+      Serial.print("MOSI: ");
+      Serial.println(Pin(GPIO_SPI_MOSI));
+      Serial.print("CLK: ");
+      Serial.println(Pin(GPIO_SPI_CLK));
+      Serial.print("DC: ");
+      Serial.println(Pin(GPIO_SPI_DC));
+      Serial.print("BUSY/MISO: ");
+      Serial.println(Pin(GPIO_SPI_MISO));
+      Serial.print("Rst: ");
+      Serial.println(Pin(GPIO_OLED_RESET));
+      const char HelloWorld[] = "Biomine S.r.l.";
+
+      display.init(9600, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+
+      display.setRotation(0);
+      display.setFont(&FreeMonoBold9pt7b);
+
+      display.setTextColor(GxEPD_BLACK);
+      int16_t tbx, tby;
+      uint16_t tbw, tbh;
+      display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+      // center the bounding box by transposition of the origin:
+      uint16_t x = ((display.width() - tbw) / 2) - tbx;
+      uint16_t y = ((display.height() - tbh) / 2) - tby;
+      display.setFullWindow();
+
+      display.firstPage(); // FILL WHITE
+      do
+      {
+        // display.drawRect(x-10,y+10, 200, -50, GxEPD_BLACK);
+        display.setCursor(x, y);
+        display.print(HelloWorld);
+      } while (display.nextPage());
+      display.hibernate();
+    }
   }
-  while (display.nextPage());
-  display.hibernate();
-
-
-
-  if (FUNC_DISPLAY_INIT_DRIVER == function) {
+  bool result = false;
+  if (FUNC_DISPLAY_INIT_DRIVER == function)
+  {
   }
   /*
   else if (epd42_init_done && (XDSP_21 == Settings->display_model)) {
@@ -91,6 +122,6 @@ bool Xdsp21(uint32_t function)
   return result;
 }
 
-#endif  // USE_DISPLAY_EPAPER42
-#endif  // USE_DISPLAY
-#endif  // USE_SPI
+#endif // USE_DISPLAY_EPAPER42
+#endif // USE_DISPLAY
+#endif // USE_SPI
